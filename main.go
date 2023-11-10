e6af2cf69bb86bcce99da36f7d8e4c87b20b0744
Natan5533 <naelramosfilho@gmail.com>
2023-11-10 16:17:56 -0300

    remove coments

--- main.go
+++ main.go
@@ -15,23 +15,4 @@ func main() {
 	LibraryRepo := repo.InitLibraryRepo(db)
 
 	LibraryRepo.Create("Kalunga", "Paulista Avenue")
-
-	// library := models.NewLibrary("Kalunga", "Paulista Avenue")
-
-	// library.Libraries.Create("Kalunga", "Paulista Avenue")
-
-	// LibraryRepo.Create("Kalunga2", "Paulista Avenue2")
-
-	// db.Create(&models.Library{
-	// 	Name:    "Kalunga",
-	// 	Address: "Paulista Avenue",
-	// })
-
-	// var library models.Library
-
-	// tx := db.First(&library, 1)
-	// if tx.Error != nil {
-	// 	slog.Error("Deu ruim")
-	// 	panic(tx.Error)
-	// }
 }
