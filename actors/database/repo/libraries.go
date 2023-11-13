285cbb9b714ff4b9d686a8dadae31281857d5536
Natan5533 <naelramosfilho@gmail.com>
2023-11-12 22:59:35 -0300

    Repo refactor

--- actors/database/repo/libraries.go
+++ actors/database/repo/libraries.go
@@ -14,14 +14,25 @@ func InitLibraryRepo(db *gorm.DB) *LibraryRepo {
 	return &LibraryRepo{db: db}
 }
 
-func (repo *LibraryRepo) Create(name string, address string) error {
-	library := models.NewLibrary(name, address)
-	tx := repo.db.Create(library)
+func (repo *LibraryRepo) Create(name, address string) (int, error) {
+	libraryModel := models.NewLibrary(name, address)
 
+	tx := repo.db.Create(libraryModel)
 	if tx.Error != nil {
 		slog.Error("Deu ruim")
-		return tx.Error
+		return 0, tx.Error
 	}
 
-	return nil
+	return int(libraryModel.ID), nil
+}
+
+func (repo *LibraryRepo) GetById(id int) (*models.Library, error) {
+	var library models.Library
+	tx := repo.db.First(&library, id)
+
+	if tx.Error != nil {
+		return nil, tx.Error
+	}
+
+	return &library, nil
 }
